d6cc412417334af5aca74662d89fcb7334b05cf1
kevinakadamian19 <kevindamianta@gmail.com>
2019-05-15 01:25:45 -0700

    Added more context to functions
    For the addListItem() function I defined a variable that would be the inserted text for the Span element. Then I appended the entire content within a single li
    
    For the checklist item; I figured that all we are changing is the class from shopping-item_checked or back to shopping-item depending on what the current status is.
    
    For the removeList Item added I am playing around with event delegation to see how we would be able to remove all listed contents that are children under the Li element.

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 function addListItem() {
     $('form').submit(function(event) {
-     
-    };
+     const insertedItem = $('input:text').val();
+        $('.shopping-list').append(`<li> <span class="shopping-item">` + insertedItem + `</span>
+            <div class="shopping-item-controls">
+              <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+              </button>
+              <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+              </button>
+            </div>
+          </li>`
+          );
+    )};
 }
 
 
 function checkListItem() {
     $('.shopping-item-toggle').on('click', function(event) {
-        
+        if ($('span').css('class') === 'shopping-item' ) {
+            this.closest('span').toggleClass('shopping-item_checked')
+        };
+        if ($('span').css('class') === 'shopping-item_checked' ){
+            this.closest('span').toggleClass('shopping-item')
+        };
     });
 } 
 
 
 function removeListItem(){
-    $('.shopping-item-delete').on('click', function(event) {
+    $('.shopping-item-delete').on('click', 'li', function(event) {
         this.remove();
     });
 }
